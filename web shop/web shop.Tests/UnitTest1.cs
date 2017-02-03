d432d55df5df0e1f94b7ef2a4d8445409055486d
Marcin Kass <marcin.kass10@gmail.com>
2017-02-03 22:47:17 +0100

    Created project

---
+++ web shop/web shop.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace web_shop.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
